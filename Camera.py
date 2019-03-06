938c40bbb9b08964db268bedd1a8d5211c8147c8
Jordan <jtilbert89@gmail.com>
2019-03-06 10:52:34 -0700

    working solution

--- Camera.py
+++ Camera.py
@@ -31,8 +31,10 @@ class Camera(object):
         """
         ### rotational transform
         # read in real world coordinates
-        new_col = np.ones((len(X), 1))
+        new_col = np.ones((X.shape[0], 1))
         hom_coords = np.append(X, new_col, 1)
+        #hom_coords = np.column_stack((X, new_col))
+        #print(hom_coords)
 
         # R yaw matrix
         R_yaw = np.matrix([[cos(p[3]), -sin(p[3]), 0, 0],
@@ -64,7 +66,7 @@ class Camera(object):
         C_mat = R_axis @ R_roll @ R_pitch @ R_yaw @ T_mat
 
         # output generalized coords
-        gen_coords = np.zeros((len(X), 3))
+        gen_coords = np.zeros((X.shape[0], 3))
         for i in range(len(gen_coords)):
             h_coord_mat = np.matrix(hom_coords[i]).T
             gen_coords[i] = (C_mat@h_coord_mat).T
@@ -145,24 +147,19 @@ class rw_coords:
         self.est = est
 
     def transform(self, guess, cam1, cam2):
-        coords1 = cam1.transforms(guess, cam1.p)
-        coords2 = cam2.transforms(guess, cam2.p)
-        u1 = coords1[0]
-        u2 = coords2[0]
-        v1 = coords2[1]
-        v2 = coords2[1]
+        u1 = cam1.transforms(np.array([guess]), cam1.p)[0, 0]
+        v1 = cam1.transforms(np.array([guess]), cam1.p)[0, 1]
+        u2 = cam2.transforms(np.array([guess]), cam2.p)[0, 0]
+        v2 = cam2.transforms(np.array([guess]), cam2.p)[0, 1]
 
-        coords = np.array([u1, v1, u2, v2])
-        return coords.flatten()
+        return np.array([u1,v1,u2,v2]).flatten()
 
     def residuals(self, guess, cam1, cam2, u_image):
 
         error = self.transform(guess, cam1, cam2) - u_image.flatten()
-
         return error
 
     def find_xopt(self, cam1, cam2, u_image):
-
         x_opt = so.least_squares(self.residuals, self.est, method='lm', args=(cam1, cam2, u_image))['x']
 
         return x_opt
@@ -175,12 +172,13 @@ img_width = 3264
 img_height = 2448
 focal_length = focal_length_35/36*img_width
 
+
 c1_coords = np.loadtxt('gcp_stereo_1.txt', delimiter=',')
 c1_cam_c = c1_coords[:, 0:2]
 c1_rw_c = c1_coords[:, 2:]
 c2_coords = np.loadtxt('gcp_stereo_2.txt', delimiter=',')
-c2_cam_c = c1_coords[:, 0:2]
-c2_rw_c = c1_coords[:, 2:]
+c2_cam_c = c2_coords[:, 0:2]
+c2_rw_c = c2_coords[:, 2:]
 
 p1_guess = [272462, 5193960, 981, 0.78, 0.2, 0]
 p2_guess = [272459, 5193880, 981, 0.75, 0.1, 0]
@@ -196,31 +194,8 @@ print(pose1, pose2)
 cam1 = Camera(focal_length, [img_width, img_height], pose1)
 cam2 = Camera(focal_length, [img_width, img_height], pose2)
 est = [272555, 5193940, 1005]
-u_image = np.array([2018.9675324675327, 1295.0324675324675, 1202.3051948051948, 1219.5259740259739])
+u_image = np.array([[2018.9675324675327, 1295.0324675324675], [1202.3051948051948, 1219.5259740259739]])
 
 coords = rw_coords(est)
 x_opt = coords.find_xopt(cam1, cam2, u_image)
 print(x_opt)
-
-# run the camera model for a given image with selected gcps
-
-# params
-# f = 3200.  # focal length
-# c = [4608., 3456.]  # sensor size
-# p = [272500, 5193700, 1000, 0.78, 0.2, 0]
-# coords_txt = 'coords.csv'
-# image = 'Clapp.png'
-#
-# coords = np.loadtxt(coords_txt, delimiter=',', skiprows=1)
-#
-# X = coords[:, 2:]
-# u_gcp = coords[:, 0:2]
-#
-# cam = Camera(f, c, p)
-# out = cam.estimate_pose(X, u_gcp)
-# print(out)
-#
-# np.savetxt('optimum_pose.txt', out, delimiter=' ')
-#
-# cam_coords = cam.transforms(X, out)
-# cam.plot_output(image, u_gcp[:,0], u_gcp[:,1], cam_coords[:,0], cam_coords[:,1])
